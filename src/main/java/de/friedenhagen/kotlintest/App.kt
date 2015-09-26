b5d3e1491b0a6a115338f7839443f3947aeb107c
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2015-09-26 22:07:06 +0200

    First data classes

--- src/main/java/de/friedenhagen/kotlintest/App.kt
+++ src/main/java/de/friedenhagen/kotlintest/App.kt
@@ -1,5 +1,12 @@
 package de.friedenhagen.kotlintest
 
+class App(val firstName: String, val name: String)
+
 fun main(args:Array<String>) {
-    println(args)
+    val person = Person("Mirko", "Hierlebizz")
+    val app: App = App("Mirko", "Hierlebizz")
+    println("Hallo Person, ${person.firstName} ${person.name}")
+    println("Hallo App, ${app.firstName} ${app.name}")
+    println(person)
 }
+
---
+++ src/main/java/de/friedenhagen/kotlintest/Person.kt
@@ -0,0 +1,3 @@
+package de.friedenhagen.kotlintest
+
+data class Person(val firstName: String, val name: String)
