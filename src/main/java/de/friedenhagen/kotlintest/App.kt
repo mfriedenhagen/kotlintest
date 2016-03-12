24f71e09d3b62b8cfef3a99c4eb0e977dde4c58b
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2016-03-12 22:57:58 +0100

    Play with snakeyaml

--- src/main/java/de/friedenhagen/kotlintest/App.kt
+++ src/main/java/de/friedenhagen/kotlintest/App.kt
@@ -1,12 +1,25 @@
 package de.friedenhagen.kotlintest
 
-class App(val firstName: String, val name: String)
+import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.composer.ComposerException
+import java.io.FileInputStream
+import java.util.*
 
 fun main(args:Array<String>) {
-    val person = Person("Mirko", "Hierlebizz", 18)
-    val app = App("Mirko", "Hierlebizz")
-    println("Hallo Person, ${person.firstName} ${person.name}")
-    println("Hallo App, ${app.firstName} ${app.name}")
-    println(person)
+    val message = parse()
+    when(message) {
+        is LinkedHashMap<*,*> -> println(message)
+        is Iterable<*> -> message.forEach { println(it) }
+        else -> println(message.javaClass)
+    }
+}
+
+private fun parse() : Any {
+    val yamlParser = Yaml()
+    try {
+        return FileInputStream("foo.yml").use { yamlParser.load(it) }
+    } catch (e: ComposerException) {
+        return FileInputStream("foo.yml").use { yamlParser.loadAll(it).toList() }
+    }
 }
 
