0398f929841818ae3424a712df8cf9c9eadf1411
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2016-03-12 23:03:56 +0100

    Add parameter parsing

--- src/main/java/de/friedenhagen/kotlintest/App.kt
+++ src/main/java/de/friedenhagen/kotlintest/App.kt
@@ -5,21 +5,22 @@ import org.yaml.snakeyaml.composer.ComposerException
 import java.io.FileInputStream
 import java.util.*
 
-fun main(args:Array<String>) {
-    val message = parse()
-    when(message) {
-        is LinkedHashMap<*,*> -> println(message)
+fun main(args: Array<String>) {
+    val filename: String = if (args.size > 0) args[0] else "foo.yml"
+    val message = parse(filename)
+    when (message) {
+        is LinkedHashMap<*, *> -> println(message)
         is Iterable<*> -> message.forEach { println(it) }
         else -> println(message.javaClass)
     }
 }
 
-private fun parse() : Any {
+private fun parse(filename: String = "foo.yml"): Any {
     val yamlParser = Yaml()
     try {
-        return FileInputStream("foo.yml").use { yamlParser.load(it) }
+        return FileInputStream(filename).use { yamlParser.load(it) }
     } catch (e: ComposerException) {
-        return FileInputStream("foo.yml").use { yamlParser.loadAll(it).toList() }
+        return FileInputStream(filename).use { yamlParser.loadAll(it).toList() }
     }
 }
 
