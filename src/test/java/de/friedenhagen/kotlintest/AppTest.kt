98cbb1b54c757c8fa9e3c6dcf2348a5d07726abd
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2016-03-14 23:28:15 +0100

    Add single YAML document

--- src/test/java/de/friedenhagen/kotlintest/AppTest.kt
+++ src/test/java/de/friedenhagen/kotlintest/AppTest.kt
@@ -6,20 +6,21 @@ import org.jetbrains.spek.api.shouldEqual
 import org.jetbrains.spek.api.shouldNotBeNull
 import org.jetbrains.spek.api.shouldThrow
 import org.junit.Assert.assertThat
+import kotlin.test.assertFailsWith
 import kotlin.test.assertNotNull
 
-class AppTest1 : Spek() {
+class App1Test : Spek() {
     init {
-        given("The App has a filename") {
-            val sut = App("src/test/resources/foo.yml")
+        given("The App gets a filename with multiple documents") {
+            val sut = App("src/test/resources/multiple.yml")
             on("checking the filename") {
                 it("should have a filename") {
-                    shouldEqual("src/test/resources/foo.yml", sut.filename)
+                    shouldEqual("src/test/resources/multiple.yml", sut.filename)
                 }
             }
             on("parsing the file") {
                 val message = sut.create()
-                it("should not be an exception") {
+                it("should not create an exception") {
                     shouldNotBeNull(message)
                 }
 
@@ -28,13 +29,28 @@ class AppTest1 : Spek() {
     }
 }
 
-class AppTest2 : Spek() {
+class App2Test : Spek() {
+    init {
+        given("The App gets a filename with one document") {
+            val sut = App("src/test/resources/single.yml")
+            on("parsing the file") {
+                val message = sut.create()
+                it("should not create an exception") {
+                    shouldNotBeNull(message)
+                }
+
+            }
+        }
+    }
+}
+class App3Test : Spek() {
     init {
         given("The App gets a wrong filename") {
             val sut = App("pom.xml")
             on("parsing the file") {
                 it("should throw an Exception") {
                     val e = {sut.create()}
+                    //assertFailsWith(IllegalArgumentException.class, e)
                     //shouldThrow(IllegalArgumentException.class, e.invoke())
                 }
             }
