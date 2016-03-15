a79c7a385e0d90f073ee4fc7d51de5a6bb2557ee
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2016-03-15 21:22:01 +0100

    Test of failing the build when the given file is not a YAML document.

--- src/test/java/de/friedenhagen/kotlintest/AppTest.kt
+++ src/test/java/de/friedenhagen/kotlintest/AppTest.kt
@@ -1,17 +1,13 @@
 package de.friedenhagen.kotlintest
 
-import org.hamcrest.core.Is
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.shouldEqual
 import org.jetbrains.spek.api.shouldNotBeNull
-import org.jetbrains.spek.api.shouldThrow
-import org.junit.Assert.assertThat
 import kotlin.test.assertFailsWith
-import kotlin.test.assertNotNull
 
 class App1Test : Spek() {
     init {
-        given("The App gets a filename with multiple documents") {
+        given("the App gets a file with multiple documents") {
             val sut = App("src/test/resources/multiple.yml")
             on("checking the filename") {
                 it("should have a filename") {
@@ -31,7 +27,7 @@ class App1Test : Spek() {
 
 class App2Test : Spek() {
     init {
-        given("The App gets a filename with one document") {
+        given("the App gets a file with one document") {
             val sut = App("src/test/resources/single.yml")
             on("parsing the file") {
                 val message = sut.create()
@@ -45,13 +41,11 @@ class App2Test : Spek() {
 }
 class App3Test : Spek() {
     init {
-        given("The App gets a wrong filename") {
+        given("the App gets a non YAML file") {
             val sut = App("pom.xml")
             on("parsing the file") {
-                it("should throw an Exception") {
-                    val e = {sut.create()}
-                    //assertFailsWith(IllegalArgumentException.class, e)
-                    //shouldThrow(IllegalArgumentException.class, e.invoke())
+                it("should throw an IllegalArgumentException") {
+                    assertFailsWith(IllegalArgumentException::class) {sut.create()}
                 }
             }
 
