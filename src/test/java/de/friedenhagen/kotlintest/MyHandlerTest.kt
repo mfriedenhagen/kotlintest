5df604d698376d4debb311ce00af1c6fd845a959
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2016-03-29 21:48:22 +0200

    Use givenData from Spek.

--- src/test/java/de/friedenhagen/kotlintest/MyHandlerTest.kt
+++ src/test/java/de/friedenhagen/kotlintest/MyHandlerTest.kt
@@ -2,18 +2,23 @@ package de.friedenhagen.kotlintest
 
 import com.sun.net.httpserver.Headers
 import com.sun.net.httpserver.HttpExchange
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.givenData
 import org.jetbrains.spek.api.shouldEqual
-import org.mockito.Mockito.`mock`
 import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.net.URI
+import java.util.*
 
 class MyHandlerTest : Spek() {
+
+    val emptyInputStream = ByteArrayInputStream(ByteArray(0))
+
     init {
         given("we recieve a GET request") {
-            val exchange = createHttpExchange(ByteInputStream())
+            val exchange = createHttpExchange(emptyInputStream)
             `when`(exchange.requestMethod).thenReturn("GET")
             val sut = MyHandler()
             on("handling the GET request") {
@@ -24,7 +29,7 @@ class MyHandlerTest : Spek() {
             }
         }
         given("we recieve a HEAD request") {
-            val exchange = createHttpExchange(ByteInputStream())
+            val exchange = createHttpExchange(emptyInputStream)
             `when`(exchange.requestMethod).thenReturn("HEAD")
             val sut = MyHandler()
             on("handling the HEAD request") {
@@ -34,11 +39,12 @@ class MyHandlerTest : Spek() {
                 }
             }
         }
-        given("we recieve a POST request") {
-            val exchange = createHttpExchange(ByteInputStream())
+
+        givenData(Arrays.asList(emptyInputStream, ByteArrayInputStream("Hello".toByteArray(charset("UTF-8"))))) {
+            val exchange = createHttpExchange(it)
             `when`(exchange.requestMethod).thenReturn("POST")
             val sut = MyHandler()
-            on("handling the POST request") {
+            on("handling the POST request with body ${it}") {
                 sut.handle(exchange)
                 it("should return something") {
                     shouldEqual("OK, method=POST", exchange.responseBody.toString())
@@ -47,7 +53,7 @@ class MyHandlerTest : Spek() {
             }
         }
         given("we recieve an invalid request") {
-            val exchange = createHttpExchange(ByteInputStream())
+            val exchange = createHttpExchange(emptyInputStream)
             `when`(exchange.requestMethod).thenReturn("OPTIONS")
             val sut = MyHandler()
             on("handling the request") {
@@ -59,10 +65,10 @@ class MyHandlerTest : Spek() {
         }
     }
 
-    private fun createHttpExchange(inputStream: ByteInputStream): HttpExchange {
+    private fun createHttpExchange(inputStream: ByteArrayInputStream): HttpExchange {
         val exchange = `mock`(HttpExchange::class.java)
         `when`(exchange.requestURI).thenReturn(URI.create("/foo"))
-        `when`(exchange.responseBody).thenReturn(ByteOutputStream())
+        `when`(exchange.responseBody).thenReturn(ByteArrayOutputStream())
         `when`(exchange.requestBody).thenReturn(inputStream)
         `when`(exchange.responseHeaders).thenReturn(Headers())
         return exchange
