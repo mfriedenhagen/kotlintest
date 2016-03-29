37d9acfbfda385b5dc91dd5f9a1bb6af649a8b76
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2016-03-29 22:00:39 +0200

    Spekify.

--- src/test/java/de/friedenhagen/kotlintest/PersonTest.kt
+++ src/test/java/de/friedenhagen/kotlintest/PersonTest.kt
@@ -1,30 +1,46 @@
 package de.friedenhagen.kotlintest
 
-import org.junit.Assert
-import org.junit.Test
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.shouldBeFalse
+import org.jetbrains.spek.api.shouldBeTrue
+import org.jetbrains.spek.api.shouldEqual
 
-class PersonTest {
+class PersonTest : Spek() {
+    init {
+        given("John Doe is underage") {
+            val sut = Person("John", "Doe")
+            on("test if isOfAge") {
+                it("should be false") {
+                    shouldBeFalse(sut.isOfAge())
+                    shouldEqual(0, sut.age)
+                }
+            }
 
-    @Test
-    fun testIt() {
-        val person = Person("John", "Doe")
-        Assert.assertEquals(0, person.age)
-    }
-
-    @Test
-    fun testItOldEnough() {
-        val person = Person("John", "Doe", age = 18)
-        Assert.assertTrue("Should be at least ${person.AGE_LIMIT}, is ${person.age}", person.isOfAge())
-    }
-
-    @Test
-    fun testToString() {
-        Assert.assertEquals("Person(firstName=John, name=Doe, age=18)", Person("John", "Doe", age = 18).toString())
-    }
-
-    @Test
-    fun testEquals() {
-        val person = Person("John", "Doe", age = 18)
-        Assert.assertEquals(person, Person("John", "Doe", age = 18))
+        }
+        given("Jane Doe is old enough") {
+            val sut = Person("Jane", "Doe", age = 18)
+            on("test if isOfAge") {
+                it("should be true") {
+                    shouldBeTrue(sut.isOfAge())
+                    shouldEqual(18, sut.age)
+                }
+            }
+        }
+        given("Jane Doe has name and firstName") {
+            val sut = Person("Jane", "Doe", age = 18)
+            on("test toString") {
+                it("should return sth.") {
+                    shouldEqual("Person(firstName=Jane, name=Doe, age=18)", sut.toString())
+                }
+            }
+        }
+        given("Jane Doe is equal") {
+            val sut = Person("Jane", "Doe", age = 18)
+            on("test is equal") {
+                it("should return sth.") {
+                    shouldEqual(Person("Jane", "Doe", age = 18), sut)
+                }
+            }
+        }
     }
 }
